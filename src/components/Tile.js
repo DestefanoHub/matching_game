2fa97c6d7f6c8794c718f9a7b4cd330dc8665ada
Andrew De Stefano <elitehunter92@gmail.com>
2024-10-25 16:24:42 -0400

    Basic home page. Removed state management for hovering the tiles and use hover pseudo class instead. This seems to fixa the issue where the tiles would sometimes not revert to a non-active look when a tile pair doesn't match.

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { reveal, selectActiveTiles } from '../store/gameSlice';
@@ -6,7 +5,6 @@ import { reveal, selectActiveTiles } from '../store/gameSlice';
 import styles from './Tile.module.css';
 
 const Tile = (props) => {
-    const [isHovered, setIsHovered] = useState(false);
     const activeTiles = useSelector(selectActiveTiles);
     const gameDispatch = useDispatch();
 
@@ -16,24 +14,10 @@ const Tile = (props) => {
         }
     };
 
-    const handleMouseOver = (event) => {
-        if(!props.tile.isActive){
-            setIsHovered(true);
-        }
-    };
-
-    const handleMouseOut = (event) => {
-        if(!props.tile.isActive){
-            setIsHovered(false);
-        }
-    };
-    
     return <button id={props.tile.id} 
         type='button'
-        className={`${styles.tile} ${isHovered && styles.hover} ${props.tile.isActive && styles.active} ${props.tile.isScored && styles.scored}`}
+        className={`${styles.tile} ${props.tile.isActive && styles.active} ${props.tile.isScored && styles.scored}`}
         onClick={handleClick}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
     >
         <span className={`${styles.tileText}`}>{props.tile.isActive && props.tile.value}</span>
     </button>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom';
+
 import styles from './Home.module.css';
 
 const Home = () => {
-    return <h1>Home Page</h1>;
+    return <section className={styles.page}>
+        <h1>Home Page</h1>
+        <div className={styles.content}>
+            <p>
+                Click on a tile to reveal the hidden word. Reveal two tiles to see if the words match. If they match, you score a 
+                point and the tiles are removed. If not, the words are hidden again. Memorize the location of the words and 
+                match all the word pairs to win! 
+            </p>
+            <Link to={'game'}>Start New Game</Link>
+        </div>
+    </section>;
 }
 
 export default Home;

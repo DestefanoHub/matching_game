16190cb7f35805fe850886f56bc0f0f16f1ac276
Andrew De Stefano <elitehunter92@gmail.com>
2024-12-12 17:10:50 -0500

    Removed 'appSlice' as this is no longer relevant. Moved firestore config to separate file and added it to gitignore.

--- src/database.js
+++ src/database.js
@@ -1,15 +1,9 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, addDoc, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { getFirestore, collection, addDoc, getDocs, query, where, orderBy, startAt, limit } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDRu2DWXPN2g92QOTnSONhEbq4m7z7SjH4",
-    authDomain: "matching-game-81f97.firebaseapp.com",
-    projectId: "matching-game-81f97",
-    storageBucket: "matching-game-81f97.firebasestorage.app",
-    messagingSenderId: "595639797585",
-    appId: "1:595639797585:web:f879f3a9ab97fafa47f570",
-    measurementId: "G-RBCF92418S",
-};
+import config from './firebaseConfig.json';
+
+const firebaseConfig = config;
 
 const app = initializeApp(firebaseConfig);
 const database = getFirestore(app);
@@ -41,12 +35,69 @@ export const getRecentGames = async () => {
     return gamesArray;
 };
 
-export const getGames = async () => {
+export const getGames = async (player, winLoss, diff, sort, page=1) => {
     const gamesArray = [];
-    const gamesQuery = query(collection(database, 'games'), orderBy('date', 'desc'));
+    const recordsPerPage = 10;
+    const recordStart = (page * recordsPerPage) - recordsPerPage;
+    let gamesQuery = collection(database, 'games');
+    
+    //optional player search
+    if(player.length){
+        gamesQuery = query(gamesQuery, where('player', '==', player));
+    }
+
+    //required win/loss filter
+    switch(winLoss){
+        case 'w': {
+            gamesQuery = query(gamesQuery, where('hasWon', '==', true));
+            break;
+        }
+        case 'l': {
+            gamesQuery = query(gamesQuery, where('hasWon', '==', false));
+            break;
+        }
+        case 'a':
+        default:
+            break;
+    }
+
+    //required difficulty filter
+    if(diff !== 0){
+        gamesQuery = query(gamesQuery, where('difficulty', '==', diff));
+    }
+
+    //required sort and order
+    switch(sort){
+        case 'sd': {
+            gamesQuery = query(gamesQuery, orderBy('points', 'desc'));
+            break;
+        }
+        case 'sa': {
+            gamesQuery = query(gamesQuery, orderBy('points', 'asc'));
+            break;
+        }
+        case 'da': {
+            gamesQuery = query(gamesQuery, orderBy('date', 'asc'));
+            break;
+        }
+        case 'dd':
+        default: {
+            gamesQuery = query(gamesQuery, orderBy('date', 'desc'));
+            break;
+        }
+    }
+
+    //pagination - ten records per page
+    gamesQuery = query(gamesQuery, limit(recordsPerPage));
+    // const pageSetup = await getDocs(gamesQuery);
+    // const firstRecord = pageSetup.docs[0];
+    // console.log(firstRecord);
+    // gamesQuery = query(gamesQuery, startAt(firstRecord), limit(recordsPerPage));
+    
     const games = await getDocs(gamesQuery);
     games.forEach((game) => {
         gamesArray.push(game.data());
     });
+
     return gamesArray;
 };

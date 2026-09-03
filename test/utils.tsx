00ec2d52b080eef75d7d69ca0659cfc027d0ef8a
DestefanoHub <elitehunter92@gmail.com>
2026-09-03 11:48:47 -0400

    Better type definitions to test utils.tsx. Added cleanup to test setup.ts to ensure a clean run for  each test.

--- test/utils.tsx
+++ test/utils.tsx
@@ -1,25 +1,31 @@
-import { type ReactElement } from 'react';
+import { type ReactElement, type ReactNode } from 'react';
 import { render, type RenderOptions } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { configureStore, type Store } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { MemoryRouter } from 'react-router';
 
 import { rootReducer } from '../src/store/store';
+import type { RootState, AppStore } from '../src/store/store';
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
-    preloadedState?: Record<string, any>;
-    store?: Store;
+    preloadedState?: Partial<RootState>;
+    store?: AppStore;
     initialRoute?: string[];
-};
+}
 
-export function renderWithProviders(ui: ReactElement, {
+export function renderWithProviders(
+    ui: ReactElement,
+    {
         preloadedState = {},
-        store = configureStore({ reducer: rootReducer, preloadedState }),
+        store = configureStore({
+            reducer: rootReducer,
+            preloadedState,
+        }),
         initialRoute = ['/'],
         ...renderOptions
     }: ExtendedRenderOptions = {}
 ) {
-    function Wrapper({ children }: { children: React.ReactNode }) {
+    function Wrapper({ children }: { children: ReactNode }) {
         return (
             <Provider store={store}>
                 <MemoryRouter initialEntries={initialRoute}>

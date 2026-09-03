3ed894c1ba8bef9f94911fef93412e13bf2dc948
DestefanoHub <elitehunter92@gmail.com>
2026-09-03 15:19:45 -0400

    Testing starting to work. Removed simple test file.

--- test/components/account/Login.test.tsx
+++ test/components/account/Login.test.tsx
@@ -1,10 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { renderWithProviders, screen, within } from '../../utils';
 
 import Header from '../../../src/components/generic/Header';
 
 describe('Login component', () => {
-    test('Modal closes upon successful login.', () => {
-        render(<Header/>);
-        screen.debug();
+    test('shows the header Login button when logged out', () => {
+        renderWithProviders(<Header />);
+        const header = screen.getByRole('banner');
+
+        const loginButton = within(header).getByRole('button', {
+            name: /login/i
+        });
+        expect(loginButton).toBeVisible();
+
+        const createAccountButton = within(header).getByRole('button', {
+            name: /create account/i
+        });
+        expect(createAccountButton).toBeVisible();
+
+        const accountText = within(header).getByText(/account:/i);
+        expect(accountText).toBeVisible();
     });
 });
--- test/simple.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "vitest";
-
-describe('Simple test scenario', () => {
-    test('a basic test case', () => {
-        expect(2+2).toBe(4);
-        expect(!true).toBeFalsy();
-    });
-});

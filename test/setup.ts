1ef107400fca2ced34e75a2e999f816c15ac414f
DestefanoHub <elitehunter92@gmail.com>
2026-09-08 17:25:10 -0400

    Polyfilled HTML dialog events for jsdom. Made sure  to clear the session storage after every test to keep the session state clean.

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,26 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
+import { afterEach, beforeEach, beforeAll } from 'vitest';
+
+beforeAll(() => {
+    if (!HTMLDialogElement.prototype.showModal) {
+        HTMLDialogElement.prototype.showModal = function () {
+            this.setAttribute('open', 'open');
+            this.setAttribute('aria-hidden', 'false');
+        };
+    }
+
+    if (!HTMLDialogElement.prototype.close) {
+        HTMLDialogElement.prototype.close = function () {
+            this.removeAttribute('open');
+            this.setAttribute('aria-hidden', 'true');
+        };
+    }
+});
+
+beforeEach(() => {
+    sessionStorage.clear();
+});
 
 afterEach(() => {
     cleanup();
